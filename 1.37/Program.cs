2475c2d2c3aa2bb79dfed0611a18a22d1731d3c1
artem <artem@DESKTOP-6MTUGES>
2022-09-25 20:29:30 +0300

    Добавьте файлы проекта.

---
+++ 1.37/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите первый и второй катеты");
+double a = double.Parse(Console.ReadLine());
+double b = double.Parse(Console.ReadLine());
+double c = Math.Sqrt((a*a)+(b*b));
+Console.WriteLine("Периметр треугольника равен: {0}", c);// задание 1.37, найти периметр треугольника по его катетам
+
